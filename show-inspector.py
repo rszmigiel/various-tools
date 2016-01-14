26f92cadb62847f93d810896ab076f4745953674
Rafal Szmigiel <rszmigie@redhat.com>
2016-01-14 21:52:22 +0100

    Making some progress. Need better test environment.

--- show-inspector.py
+++ show-inspector.py
@@ -1,6 +1,10 @@
 import json
+import os
 import math
 import sys
+from ironicclient import client as iclient
+from swiftclient import client as sclient
+
 
 def convertSize(size):
    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
@@ -13,16 +17,60 @@ def convertSize(size):
        return '0B'
 
 
-fname = sys.argv[1]
+def getConfig():
+    kwargs = {'os_username': None,
+              'os_password': None,
+              'os_auth_url': None,
+              'os_tenant_name': None
+              }
 
-f = open(fname)
-data = json.load(f)
-f.close()
+    for variable in kwargs.keys():
+        try:
+            kwargs[variable] = os.environ[variable.upper()]
+        except KeyError:
+            print "Variable ${variable} is not set.".format(variable=variable.upper())
+            sys.exit(1)
+
+    return(kwargs)
+
+
+def getIronicNodes(kwargs):
+    uuids = []
+    ironic = iclient.get_client(1, **kwargs)
+    nodes = ironic.node.list()
+    for node in nodes:
+        uuids.append(node.uuid)
+    return uuids
+
+def getIntrospectionData(**kwargs):
+    swift = sclient.Connection(
+        user = kwargs['os_username'],
+        key = kwargs['os_password'],
+        authurl = kwargs['os_auth_url'],
+        tenant_name = 'service',
+        auth_version = 2
+        )
+
+    print swift.get_account()
+
+
+kwargs = getConfig()
+uuids = getIronicNodes(kwargs)
+print uuids
+for uuid in uuids:
+    getIntrospectionData(uuid=uuid, **kwargs)
+
+
+
+#fname = sys.argv[1]
+
+#f = open(fname)
+#data = json.load(f)
+#f.close()
 
 ram_total = 0
 disks = []
 cpus = {} 
-
 
 for i in data:
 	if i[0] == 'memory' and i[2] == 'size':
