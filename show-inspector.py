d2278f3aa81cb60e3efe94bb9418fe2c7e1454f2
Rafal Szmigiel <rszmigie@redhat.com>
2016-01-19 13:14:21 +0100

    Adding support for NIC data.

--- show-inspector.py
+++ show-inspector.py
@@ -63,24 +63,29 @@ def prettyPrintNodeData(data):
     ram_total = 0
     disks = []
     cpus = {}
+    nics = {}
     ipmi_mac = None
     ipmi_ip = None
 
     for i in data_json:
         if i[0] == 'memory' and i[2] == 'size':
-            ram_total += int(i[3])
-
+            m_total += int(i[3])
         if i[0] == 'disk' and i[2] == 'size':
             disks.append([i[1], i[3]])
-
         if i[0] == 'cpu' and i[2] == 'number':
             cpus[i[1]] = i[3]
-
         if i[0] == 'ipmi' and i[2] == 'mac-address':
             ipmi_mac = i[3]
-
         if i[0] == 'ipmi' and i[2] == 'ip-address':
             ipmi_ip = i[3]
+        if i[0] == 'network':
+            nic_name = i[1]
+            if nic_name not in nics.keys():
+                nics[nic_name] = {}
+            if i[2] == 'link':
+                nics[nic_name]['link'] = i[2]
+            if i[2] == 'speed':
+                nics[nic_name]['speed'] = i[2]
 
     print "Node {uuid} ({ipmi_ip} / {ipmi_mac}): ".format(uuid=uuid, ipmi_mac=ipmi_mac, ipmi_ip=ipmi_ip)
     print "  CPU: physical: {physical} / logical: {logical}".format(physical=cpus['physical'], logical=cpus['logical'])
@@ -88,6 +93,10 @@ def prettyPrintNodeData(data):
     print "  Disks:"
     for disk in disks:
         print "    - /dev/{drive}: {disk_size}".format(drive=disk[0], disk_size=disk[1])
+    print "  NICs:"
+    for nic in nics.keys():
+        print "{nic}: link up: {link}\tlink speed: {speed}".format(nic=nic,
+                link=nics[nic]['link'], speed=nics[nic]['speed'])
     print "\n"
 
 
@@ -99,7 +108,6 @@ if __name__ == "__main__":
     for uuid in uuids:
         data = getIntrospectionData(uuid=uuid, **kwargs)
         data_json = json.loads(data)
-        
         prettyPrintNodeData(data_json)
 
 
